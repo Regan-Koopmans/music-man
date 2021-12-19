65030eb168bc6b818ac87eeac99d7d4c62d70361
Regan-Koopmans <regan.koopmans@teampicnic.com>
2021-12-19 15:29:30 +0100

    Update music definition

--- static/logic/music.pl
+++ static/logic/music.pl
@@ -18,5 +18,17 @@ major_triad_notes(Base, MajorThird, PerfectFifth) :-
     IndexOfThird is (IndexOfBase + 4) mod NumberOfNotes,
     IndexOfFifth is (IndexOfBase + 7) mod NumberOfNotes.
 
+minor_triad_notes(Base, MinorThird, PerfectFifth) :-
+    notes(Notes),
+    length(Notes, NumberOfNotes),
+    member(Base, Notes),
+    member(MinorThird, Notes),
+    member(PerfectFifth, Notes),
+    nth0(IndexOfBase, Notes, Base),
+    nth0(IndexOfThird, Notes, MinorThird),
+    nth0(IndexOfFifth, Notes, PerfectFifth),
+    IndexOfThird is (IndexOfBase + 3) mod NumberOfNotes,
+    IndexOfFifth is (IndexOfBase + 7) mod NumberOfNotes.
 
 major_triad(X) :- major_triad_notes(A, B, C), X = [A, B, C].
+minor_triad(X) :- minor_triad_notes(A, B, C), X = [A, B, C].
