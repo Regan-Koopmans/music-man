b5fad98c6ab4205e072da46ac7e7a302ad742fc0
Regan-Koopmans <regan.koopmans@teampicnic.com>
2021-12-19 12:24:36 +0100

    Reimagine as web

--- main.go
+++ main.go
@@ -1,64 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"github.com/jroimartin/gocui"
-	"log"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
-func keybindings(g *gocui.Gui) error {
-
-	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
-		return err
-	}
-
-	if err := g.SetKeybinding("", gocui.KeyEnter, gocui.ModNone, handleEnter); err != nil {
-		return err
-	}
-	return nil
-}
-
 func main() {
-	g, err := gocui.NewGui(gocui.Output256)
-	if err != nil {
-		log.Panicln(err)
-	}
-	defer g.Close()
-
-	g.SetManagerFunc(layout)
-	if err := keybindings(g); err != nil {
-		log.Panicln(err)
-		return
-	}
-
-	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
-		log.Panicln(err)
-	}
-}
-
-func layout(g *gocui.Gui) error {
-	maxX, maxY := g.Size()
-
-	if _, err := g.SetView("main", 0, 0, maxX, maxY); err != nil {
-		if err != gocui.ErrUnknownView {
-			return err
-		}
-
-	}
-	if v, err := g.SetView("prompt", maxX/2-8, maxY/2, maxX/2+9, maxY/2+2); err != nil {
-		if err != gocui.ErrUnknownView {
-			return err
-		}
-		fmt.Fprintln(v, "Music Man v0.0.1")
-		g.SetCurrentView("prompt")
-	}
-	return nil
-}
-
-func handleEnter(g *gocui.Gui, view *gocui.View) error {
-	return nil
-}
-
-func quit(_ *gocui.Gui, _ *gocui.View) error {
-	return gocui.ErrQuit
+	r := gin.Default()
+	r.Static("/static", "./static")
+	r.GET("/", func(c *gin.Context) {
+		c.Redirect(http.StatusMovedPermanently, "/static/")
+	})
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	_ = r.Run()
 }
