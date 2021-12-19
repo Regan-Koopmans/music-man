e4e52881f4ae64786d1f19904e74db6d92196573
Regan-Koopmans <regan.koopmans@teampicnic.com>
2021-12-19 14:03:16 +0100

    Update music definition

--- static/logic/music.pl
+++ static/logic/music.pl
@@ -3,3 +3,7 @@
 % Western music divides frequency into 12 tones. They are named thusly:
 notes([a, a_sharp, b, c, c_sharp, d, d_sharp, e, f, f_sharp, g, g_sharp]).
 
+% These notes can be combined in various ways.
+% One such way is a "major triad"
+
+major_triad(X) :- notes(Notes), member(X, Notes).
