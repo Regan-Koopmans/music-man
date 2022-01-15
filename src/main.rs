1dd78363e90487d880b12cd975431656100e099b
Regan-Koopmans <regan.koopmans@teampicnic.com>
2022-01-15 18:23:36 +0100

    Add logger

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+#[macro_use]
+extern crate log;
+
 use actix_files::Files;
 use actix_web::{App, HttpServer};
+use log::info;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -8,6 +12,7 @@ async fn main() -> std::io::Result<()> {
         HttpServer::new(|| App::new().service(Files::new("/", "./static/").prefer_utf8(true)))
             .bind(("127.0.0.1", PORT))?
             .run();
-    println!("Listening on 127.0.0.1:{}", PORT);
+    env_logger::init();
+    info!("Listening on 127.0.0.1:{}", PORT);
     result.await
 }
